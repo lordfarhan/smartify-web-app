b549075cf0c8b799b90958d4ee052cc8d3f2fc7f
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2020-07-13 15:03:39 +0700

    Added youtube plugin for ckeditor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -3,8 +3,11 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+  // Define changes to default configuration here. For example:
+  config.language = 'id';
+  // config.uiColor = '#AADC6E';
+  config.extraPlugins = 'youtube';
+  config.youtube_responsive = true;
+  config.allowedContent = true;
 };
