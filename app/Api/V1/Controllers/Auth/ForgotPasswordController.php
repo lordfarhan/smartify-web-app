06fe4956fdfc05acd710b5a66c32b12de21aa203
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2020-09-19 10:57:13 +0700

    Fixed forgot password API.

--- app/Api/V1/Controllers/Auth/ForgotPasswordController.php
+++ app/Api/V1/Controllers/Auth/ForgotPasswordController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,25 +20,37 @@ class ForgotPasswordController extends Controller
    */
   public function recover(Request $request)
   {
+    $credentials = $request->only('email');
+
+    $rules = [
+      'email' => 'required|email',
+    ];
+
+    $validator = Validator::make($credentials, $rules);
+    
+    if ($validator->fails()) {
+      $errorString = implode(",", $validator->messages()->all());
+      return response()->json(['success' => false, 'message' => $errorString], 428);
+    }
+
     $user = User::where('email', $request->email)->first();
     if (!$user) {
-        $error_message = "Your email address was not found.";
-        return response()->json(['success' => false, 'message' => $error_message], 401);
+      $error_message = "Your email address was not found.";
+      return response()->json(['success' => false, 'message' => $error_message], 404);
     }
 
     try {
-        Password::sendResetLink($request->only('email'), function (Message $message) {
-            $message->subject('Your Password Reset Link');
-        });
-
+      Password::sendResetLink($request->only('email'), function (Message $message) {
+        $message->subject('Your Password Reset Link');
+      });
     } catch (\Exception $e) {
-        //Return with error
-        $error_message = $e->getMessage();
-        return response()->json(['success' => false, 'message' => $error_message], 401);
+      //Return with error
+      $error_message = $e->getMessage();
+      return response()->json(['success' => false, 'message' => $error_message], 500);
     }
 
     return response()->json([
-        'success' => true, 'message'=> 'A reset email has been sent! Please check your email.'
+      'success' => true, 'message' => 'A reset email has been sent! Please check your email.'
     ]);
   }
 }
